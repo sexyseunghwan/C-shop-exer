81b9815919725c8a8e6333eeb153cc568ec83980
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2020-12-29 23:38:07 +0900

    2020-12-29

---
+++ JDBC/DBUTIL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace JDBC
+{
+    class DBUTIL
+    {
+        private SqlConnection conn = null;
+
+        public SqlConnection open(String host, String db, String id, String pw)
+        {
+            String ConnectionString = "Server=" + host + ";database=" + db + ";uid=" + id + ";pwd=" + pw + ";";
+
+
+            try
+            {
+                conn.ConnectionString = ConnectionString;
+                
+                return conn;
+            }
+            catch(Exception e)
+            {
+                System.Console.WriteLine("접속 자체에 문제가 생김");
+            }
+
+            return null;
+        }
+
+        
+
+    }
+}
---
+++ JDBC/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace JDBC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBUTIL util = new DBUTIL();
+            SqlConnection conn = null;
+
+            try
+            {
+                conn = util.open("localhost","ADMIN","sa","java1234");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                System.Console.WriteLine("문제");
+            }
+
+
+
+        }
+    }
+}
