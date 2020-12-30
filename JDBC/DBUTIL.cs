47abfc626a77dd22a1c9cf4213f0689cb388d880
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2020-12-30 22:21:19 +0900

    2020-12-30

--- JDBC/DBUTIL.cs
+++ JDBC/DBUTIL.cs
@@ -9,28 +9,26 @@ namespace JDBC
 {
     class DBUTIL
     {
-        private SqlConnection conn = null;
+        private SqlConnection conn;
 
         public SqlConnection open(String host, String db, String id, String pw)
         {
-            String ConnectionString = "Server=" + host + ";database=" + db + ";uid=" + id + ";pwd=" + pw + ";";
-
-
+            String ConnectionString = "Server=" + host + ",8095;database=" + db + ";uid=" + id + ";pwd=" + pw + ";";
             try
             {
+                SqlConnection conn = new SqlConnection();
                 conn.ConnectionString = ConnectionString;
-                
+                conn.Open();
+
                 return conn;
             }
             catch(Exception e)
             {
+                Console.WriteLine(e.Message);
                 System.Console.WriteLine("접속 자체에 문제가 생김");
             }
 
             return null;
         }
-
-        
-
     }
 }
--- JDBC/Program.cs
+++ JDBC/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
+
 
 namespace JDBC
 {
@@ -17,6 +19,41 @@ namespace JDBC
             try
             {
                 conn = util.open("localhost","ADMIN","sa","java1234");
+
+                //SqlCommand scom = new SqlCommand();
+
+                //scom.Connection = conn;
+
+                //그냥 기본적인 SELECT 문
+                //scom.CommandText = "SELECT name,city FROM dbo.TBLINSA";
+
+                //SqlDataReader sdr = scom.ExecuteReader();
+
+                //while(sdr.Read())
+                //{
+                //    Console.WriteLine("이름 : {0}, 사는 곳 : {1}",sdr["name"].ToString(), sdr["city"].ToString());
+                //}
+
+                //프로시저 호출
+                SqlCommand scom = new SqlCommand("sh_test1", conn);
+                scom.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter pinput = new SqlParameter("@buseo", System.Data.SqlDbType.VarChar);
+                pinput.Direction = ParameterDirection.Input;
+                pinput.Value = "개발부";
+                scom.Parameters.Add(pinput);
+
+                SqlDataReader sdr = scom.ExecuteReader();
+
+                while (sdr.Read())
+                {
+                    Console.WriteLine("이름 : {0} 부서 : {1} 직위 : {2}", sdr["name"].ToString(), sdr["buseo"].ToString(), sdr["jikwi"].ToString());
+                }
+
+                //뭐 이정도는 쉬운데
+
+                conn.Close();//객체 닫아주기
+
             }
             catch(Exception e)
             {
@@ -24,7 +61,8 @@ namespace JDBC
                 System.Console.WriteLine("문제");
             }
 
-
+            
+            System.Console.ReadLine();
 
         }
     }
